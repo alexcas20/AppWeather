1bfea8082f6e5734f20c57d6f9b1c5ed74468b38
alexcas20 <alexcalvillosalas@gmail.com>
2023-07-27 19:37:44 -0600

    add enviroment to service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private direccion = environment._APIurl;
+  private key = environment._APIkey;
+  
+
+  constructor(private http: HttpClient) { }
 }
